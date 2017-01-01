5748fb1284f66f1ae5073b477caaaaff9d515e61
Harms <543232@bah.com>
2017-01-01 17:11:21 -0500

    updating test.js and deleting pj.js

--- config/pj.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Test environment settings
-let config = {};
-
-// Use special purpose test database
-config.databaseUrl = 'postgres://djxlbnxd:rijGlhD5AkNnJIjFNnkNUgbJKFIOYPt9@elmer.db.elephantsql.com:5432/djxlbnxd';
-
-module.exports = config;
