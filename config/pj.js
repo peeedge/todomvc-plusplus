d8c2ca6f8d84ce4873e3300e5b4380597230cb52
peeedge <peeedge@gmail.com>
2016-12-31 22:43:04 -0500

    delete this file

---
+++ config/pj.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Test environment settings
+let config = {};
+
+// Use special purpose test database
+config.databaseUrl = 'postgres://djxlbnxd:rijGlhD5AkNnJIjFNnkNUgbJKFIOYPt9@elmer.db.elephantsql.com:5432/djxlbnxd';
+
+module.exports = config;
