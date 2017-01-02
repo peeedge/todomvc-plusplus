a9a810b68d98cff37da4021fe4488e9ac44c551a
Harms <543232@bah.com>
2017-01-01 23:41:40 -0500

    adding user.js and updating test.js config databaseUrl

--- config/user.js
+++ config/user.js
@@ -4,6 +4,6 @@
 let config = {};
 
 // Use special purpose test database
-config.databaseUrl = 'postgres://localhost:5432/todos-test';
+config.databaseUrl = 'postgres://djxlbnxd:rijGlhD5AkNnJIjFNnkNUgbJKFIOYPt9@elmer.db.elephantsql.com:5432/djxlbnxd';
 
 module.exports = config;
